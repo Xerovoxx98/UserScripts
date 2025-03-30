6beaf28cba60a903932b19ea96e4ced87b145adb
Xerovoxx98 <github@loader.au>
2025-03-30 15:11:54 +1000

    Create auto_scroll_to_bottom.js

---
+++ auto_scroll_to_bottom.js
@@ -0,0 +1,15 @@
+// ==UserScript==
+// @name         Auto Scroll to Bottom
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  Keep specified tabs scrolled to the bottom
+// @author       Xerovoxx98
+// @match        ^https:\/\/cronicle\.loader\.au\/console\?id=.*$
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+    window.scrollTo(0, document.body.scrollHeight);
+    setInterval(() => window.scrollTo(0, document.body.scrollHeight), 1000); // Adjust the interval as needed
+})();
