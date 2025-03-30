e05a96817490a5d3c1abd55a15bdac2d6d6b7fea
Xerovoxx98 <github@loader.au>
2025-03-30 16:40:00 +1000

    Update proxmox_firacode.js

--- proxmox_firacode.js
+++ proxmox_firacode.js
@@ -1,27 +1,34 @@
 // ==UserScript==
 // @name         Use FiraCode Nerd Font (without breaking icons)
 // @namespace    http://tampermonkey.net/
-// @version      0.7
-// @description  Change font to FiraCode Nerd Font while preserving icon fonts
+// @version      0.8
+// @description  Change font to FiraCode Nerd Font on Proxmox, while preserving icon fonts
 // @author       Xerovoxx98
 // @match        https://shadowcore.loader.au*
 // @grant        GM_addStyle
 // ==/UserScript==
 
 GM_addStyle(`
-  /* Import the FiraCode Nerd Font styling */
+  /* Import FiraCode Nerd Font styling */
   @import url('https://cdn.jsdelivr.net/npm/nerd-fonts/css/font-awesome.css');
 
-  /* Apply the font to most elements */
-  body, body * {
+  /* 1. Apply desired font to most text elements, but skip those that are most likely icons.
+       Adjust the :not() selectors as needed based on the target page’s markup. */
+  body *:not([class*="fa"]):not([class*="icon"]):not(i):not(span.fa) {
       font-family: "Fira Code", "FiraCode Nerd Font", monospace !important;
   }
 
-  /* Exclude common icon selectors */
+  /* 2. Reinstate the default icon font on common icon selectors.
+       Replace 'FontAwesome' below with the actual default icon font Proxmox uses if needed. */
   .fa,
+  .fas,
+  .far,
+  .fal,
+  .fab,
+  [class*="fa-"],
   [class*="icon"],
-  i,
+  i[class*="fa"],
   span.fa {
-      font-family: initial !important;
+      font-family: 'FontAwesome', sans-serif !important;
   }
 `);
