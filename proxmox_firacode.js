b3d0993777884a6277f882df2989c4b0bd1de717
Xerovoxx98 <github@loader.au>
2025-03-30 17:10:00 +1000

    Update proxmox_firacode.js

--- proxmox_firacode.js
+++ proxmox_firacode.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Primary Font as Fira Code with Icon Fallbacks
 // @namespace    http://tampermonkey.net/
-// @version      1.5
+// @version      1.6
 // @description  Set FiraCode as the primary font for Proxmox, with fallbacks to icons and defaults
 // @author       Xerovoxx98
 // @match        https://shadowcore.loader.au*
@@ -15,14 +15,14 @@
         @import url('https://cdn.jsdelivr.net/npm/nerd-fonts/css/FiraCode.css');
 
         /* Add your classes here, separated by commas */
-        .fira-code-class, code, pre, kbd, samp {
-            font-family: 'Fira Code Nerd Font', monospace !important;
+        .x-body {
+            font-family: 'Fira Code Nerd Font', helvetica, arial, verdana, sans-serif;
         }
 
         /* Example of targeting specific elements */
         /*
         .some-other-class {
-            font-family: 'Fira Code Nerd Font', monospace !important;
+            font-family: 'Fira Code Nerd Font', helvetica, arial, verdana, sans-serif;
         }
         */
 
