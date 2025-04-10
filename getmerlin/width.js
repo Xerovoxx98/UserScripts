f764d7de6647aa3144460d20893e7a4b661bd318
Xerovoxx98 <github@loader.au>
2025-04-10 13:04:05 +1000

    Update width.js

--- getmerlin/width.js
+++ getmerlin/width.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Expand Chat Response Box
 // @namespace    http://tampermonkey.net/
-// @version      0.6
+// @version      0.8
 // @description  Expands the width of the chat response box on the Chat - Merlin AI page
 // @author       Xerovoxx98
 // @match        https://www.getmerlin.in/chat*
@@ -13,12 +13,8 @@
   const style = document.createElement('style');
 
   style.textContent = `
-    @media (min-width: 768px) {
-      .relative.mx-auto.grid.w-full.content-start {
-        max-width: none !important;
-      }
-    }
+.relative.mx-auto.grid.w-full.content-start {max-width: none !important;}
+.rounded-lg.absolute.bottom-offset-down-4 {max-width: none !important;padding-right: 30px;padding-left: 30px;}
   `;
-
   document.head.appendChild(style);
 })();
