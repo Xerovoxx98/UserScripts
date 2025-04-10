6f227a5bea3534262390eb99aafc04b7bc04c939
Xerovoxx98 <github@loader.au>
2025-04-10 15:48:34 +1000

    Update firacode.js

--- proxmox/firacode.js
+++ proxmox/firacode.js
@@ -13,8 +13,6 @@
 
     GM_addStyle(`
         @import url('https://cdn.jsdelivr.net/npm/nerd-fonts/css/FiraCode.css');
-
-        /* Add your classes here, separated by commas */
         .x-body, .x-tree-node-text, .x-grid-item, .x-btn-inner-default-toolbar-small, .x-btn-inner-default-small, .x-column-header, .x-menu-item-text-default, .x-form-text-default, .x-boundlist-item, .x-tab-inner-default, .terminal, .x-form-text-default {
             font-family: 'Fira Code Nerd Font', monospace !important;
         }
